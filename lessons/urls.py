1479eb88cbf3beb275d4de5365ab6bd48eb91e30
naitmag <me.yarmolovich@gmail.com>
2024-10-08 12:56:06 +0300

    Created API get_schedule for weekly schedule Bug fix

--- lessons/urls.py
+++ lessons/urls.py
@@ -6,5 +6,6 @@ app_name = 'lessons'
 
 urlpatterns = [
     path('', views.ScheduleView.as_view(), name="schedule"),
-    path('lesson/<int:lesson_id>', views.LessonView.as_view(), name="lesson")
+    path('lesson/<int:lesson_id>', views.LessonView.as_view(), name="lesson"),
+    path('get_schedule/', views.GetWeekScheduleView.as_view(), name="get_schedule")
 ]
--- lessons/views.py
+++ lessons/views.py
@@ -1,3 +1,8 @@
+from collections import defaultdict
+
+from django.db.models import QuerySet
+from django.http import JsonResponse
+from django.views import View
 from django.views.generic import ListView, DetailView
 
 from lessons.models import Lesson
@@ -32,3 +37,30 @@ class LessonView(DetailView):
         context['related_lessons'] = Lesson.objects.filter(teacher__icontains=teacher).exclude(id=self.object.id)
         context['title'] = self.object.name
         return context
+
+
+class GetWeekScheduleView(View):
+    def get(self, request):
+        week_number = request.GET.get("week_number")
+
+        lessons = Lesson.objects.filter(start__lte=week_number, end__gte=week_number)
+
+        schedule = defaultdict(lambda: defaultdict(list))
+
+        for lesson in lessons:
+            schedule[lesson.get_day_name()][lesson.get_time()].append({
+                'id': lesson.pk,
+                'lesson_type': lesson.lesson_type,
+                'name': lesson.name,
+                'teacher': lesson.teacher,
+                'subgroup': lesson.subgroup,
+            })
+        #temp value
+        max_week = 20
+
+        response_data = {
+            'schedule': dict(schedule),
+            'max_week': max_week
+        }
+
+        return JsonResponse(response_data)
