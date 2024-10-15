6fea5d2516a4ec4965117257194d8145f2248662
naitmag <me.yarmolovich@gmail.com>
2024-10-15 12:36:49 +0300

    Added models admin-panel view

--- lessons/admin.py
+++ lessons/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from lessons.models import Lesson
 
-admin.site.register(Lesson)
+
+# admin.site.register(Lesson)
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['name', 'day', 'number', 'get_group_number', 'start', 'end']
+    search_fields = ['name', 'teacher']
+    list_filter = ['name', 'day', 'number', 'start', 'end']
+
--- users/admin.py
+++ users/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 
 from users.models import User, Group
 
-admin.site.register(Group)
-admin.site.register(User)
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['number', 'specialization', 'department']
+    search_fields = ['number', 'specialization']
+    list_filter = ['number', 'department']
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['get_full_name', 'get_group_number']
+    search_fields = ['last_name', 'first_name', 'middle_name']
+    list_filter = ['group']
